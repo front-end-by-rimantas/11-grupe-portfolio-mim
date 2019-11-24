65f9295824caaa6859ec2f5b22bfc39bc51bb254
Novicor <m.dubovikas@gmail.com>
2019-11-24 23:54:15 +0200

    Gallery active line

--- js/functions.js
+++ js/functions.js
@@ -46,17 +46,17 @@ function renderGallery( list ) {
     for ( let i=0; i<list.length; i++) {
         const tags = list[i].tags;
         for ( let j=0; j<tags.length; j++) {
-            const tag = tags[j];
+            const tag = tags[j].toLowerCase();
             if ( uniqueTags.indexOf(tag) === -1 ) {
                 uniqueTags.push(tag);
             }
         }
     }
     
-    filterHTML += `<div class="item active">All</div>`;
-    filterHTML += `<div class="item">${uniqueTags[4]}</div>`;
+    filterHTML += `<div class="item active" data-tag="all">All</div>`;
+    filterHTML += `<div class="item" data-tag="web">${uniqueTags[4]}</div>`;
     for ( let i=0; i<uniqueTags.length-1; i++) {
-        filterHTML += `<div class="item">${uniqueTags[i]}</div>`;
+        filterHTML += `<div class="item" data-tag="${uniqueTags[i]}">${uniqueTags[i]}</div>`;
     }
     
 
@@ -64,7 +64,7 @@ function renderGallery( list ) {
     for ( let i=0; i<list.length; i++ ) {
         const work = list[i];
 
-        listHTML += `<div class="work">
+        listHTML += `<div class="work show">
                         <img src="./img/work/${work.photo}">
                         <div class="ant">
                             <a href="#" class="btn-round fa fa-link"></a>
@@ -83,10 +83,62 @@ function renderGallery( list ) {
                     ${listHTML}
                 </div>
             </div>`;
+    
+    DOM.innerHTML = HTML;
 
-    return DOM.innerHTML = HTML;
+    const filterItems = DOM.querySelectorAll('.filter > .item');
+    
+    for ( let i=0; i<filterItems.length; i++ ) {
+        filterItems[i].addEventListener('click', updateGallery);
+    }
+    for ( let i=0; i<filterItems.length; i++) {
+        filterItems[i].addEventListener('click', activeGallery);
+    }
+    return; 
 }
 
+function activeGallery( ev ) {
+    const clicked = ev.target;
+    const clickedTag = ev.target.dataset.tag;
+    const DOMitem = document.querySelector('.gallery > .filter > .item.active');
+
+    if ( clickedTag ) {
+        DOMitem.classList.remove('active');
+        clicked.classList.add('active');
+    }
+    return;
+}
+
+function updateGallery( event ) {
+    const filter = event.target.dataset.tag;
+    const DOMworks = document.querySelectorAll('.gallery > .list > .work');
+
+    if ( filter === 'all') {
+        for ( let i=0; i<DOMworks.length; i++ ) {
+            DOMworks[i].classList.add('show');
+        }
+        return;
+    }
+
+    for ( let i=0; i<works.length; i++) {
+        const work = works[i];
+        let show = false;
+        
+        for ( let j=0; j<work.tags.length; j++ ) {
+            const tag = work.tags[j].toLowerCase();
+
+            if ( filter === tag ) {
+                show = true;
+            }
+        }
+        if ( show ) {
+            DOMworks[i].classList.add('show');
+        } else {
+            DOMworks[i].classList.remove('show');
+        }
+    }
+    return;
+}
 
 // services
 function renderBlocks( list ) {
